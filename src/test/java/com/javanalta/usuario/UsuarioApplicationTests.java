7bec07984b81b3b6355022ab50d87335730e0ec3
lidiane <lidianialbertini@hotmail.com>
2025-05-21 16:52:49 -0300

    commit inicial

---
+++ src/test/java/com/javanalta/usuario/UsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javanalta.usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
